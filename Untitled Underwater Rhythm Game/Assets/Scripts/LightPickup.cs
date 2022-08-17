342f934fc09d79956825dc71757c187210c81975
PetersonNormil <petersonnormil@gmail.com>
2022-08-17 14:40:43 -0400

    light and gate system

---
+++ Untitled Underwater Rhythm Game/Assets/Scripts/LightPickup.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPickup :MonoBehaviour, ICollectible
+{
+    public GameObject lightObj;
+
+    private void Start()
+    {
+        lightObj.SetActive(false);
+    }
+    public void Collect()
+    {
+        //get the players light object and change its brightness real quick
+        Debug.Log("Picked up a light collect, chagning brightness ");
+        FindObjectOfType<LightState>().playerLightState = LightStates.FirstLightState;
+        lightObj.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
